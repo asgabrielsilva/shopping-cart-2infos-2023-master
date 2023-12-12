7f55055c9afaaaa42363619108142013ee698adf
r3yfogui <gabrielhmachado06@hotmail.com>
2023-12-12 16:27:19 -0300

    footer básico

--- src/App.vue
+++ src/App.vue
@@ -52,6 +52,28 @@ window.addEventListener("scroll", reveal);
     <router-view />
   </main>
   
+
+<footer style="color: #fff;" class="bg-black rounded-lg shadow white:bg-white-800">
+    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+      <span class="text-sm text-white-500 sm:text-center white:text-white-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Guaxinins™</a>. All Rights Reserved.
+    </span>
+    <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-white-500 dark:text-white-400 sm:mt-0">
+        <li>
+            <a href="#" class="hover:underline me-4 md:me-6">About</a>
+        </li>
+        <li>
+            <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
+        </li>
+        <li>
+            <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
+        </li>
+        <li>
+            <a href="#" class="hover:underline">Contact</a>
+        </li>
+    </ul>
+    </div>
+</footer>
+
 </template>
 
 <style scoped>
